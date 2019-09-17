6d1ae3cf7b96a1d8a384c5106a6ececf0042b25e
draytonh <54688559+draytonh@users.noreply.github.com>
2019-09-17 13:14:22 -0700

    Create generate_cloud.sh
    Executable file

---
+++ generate_cloud.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+#perl -MNet::FTP -e \
+   # '$ftp = new Net::FTP("ftp.ncbi.nlm.nih.gov", Passive => 1);
+    # $ftp->login; $ftp->binary;
+     #$ftp->get("/entrez/entrezdirect/edirect.tar.gz");'
+  #gunzip -c edirect.tar.gz | tar xf -
+ # rm edirect.tar.gz
+ #builtin exit
+  #export PATH=${PATH}:$HOME/edirect >& /dev/null || setenv PATH "${PATH}:$HOME/edirect"
+  #./edirect/setup.sh
+
+#echo "export PATH=\$PATH:\$HOME/edirect" >> $HOME/.bash_profile
+
+if [ -z $1 ]; then
+    echo "Help: Please enter your search terms after the script to generate a word cloud";
+else
+esearch -db pubmed -query "$1 $2 $3" |   efetch -format abstract > pubmed.txt
+
+#pip install wordcloud
+
+wordcloud_cli --text pubmed.txt --imagefile wordcloud.png;
+fi
